47e84134b4d2711b2c39c4f55b5f059d7b7f4bfe
Alvaro <varofv66@gmail.com>
2014-11-12 19:18:47 +0100

    Creada clase NumberDisplay con con constructor que recime por parametro el limite del display  e inicializa el valor en 0

---
+++ NumberDisplay.java
@@ -0,0 +1,30 @@
+
+/**
+ * Write a description of class NumberDisplay here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class NumberDisplay
+{
+    // instance variables - replace the example below with your own
+    private int value;
+    private int limit;
+    /**
+     * Constructor for objects of class NumberDisplay
+     */
+    public NumberDisplay(int newLimit)
+    {
+        // initialise instance variables
+        value = 0;
+        limit = newLimit;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    
+}
